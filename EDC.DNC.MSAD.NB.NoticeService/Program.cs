a1945e484f252d7dcb6694b1936452b5b84960e5
EdisonZhou <edisonchou@xique.com>
2020-06-16 20:53:51 +0800

    filter wtf man**fe key words

---
+++ EDC.DNC.MSAD.NB.NoticeService/Program.cs
@@ -0,0 +1,45 @@
+﻿using EDC.DNC.MSAD.Common;
+using EDC.DNC.MSAD.NB.NoticeService;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace EDC.DNC.MSAD.NB.NoticeService
+{
+    public class Program
+    {
+        public static string IP;
+        public static int Port;
+
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            IP = config["ip"];
+            Port = Convert.ToInt32(config["port"]);
+
+            if (string.IsNullOrEmpty(IP))
+            {
+                IP = NetworkHelper.LocalIPAddress;
+            }
+
+            if (Port == 0)
+            {
+                Port = NetworkHelper.GetRandomAvaliablePort();
+            }
+
+            return WebHost.CreateDefaultBuilder(args)
+                            .UseStartup<Startup>()
+                            //.UseUrls($"http://{IP}:{Port}")
+                            .Build();
+        }
+    }
+}
